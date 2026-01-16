e287faa9e47160200a52d8679294312b6a1823af
raj54937532-bit <raj54937532@gmail.com>
2026-01-17 02:53:55 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-# Telegram configuration
-
-# Telegram Bot Token (from BotFather)
-BOT_TOKEN = "8533843459:AAFahxEIhkIbwTEvDCa1SZVpIaeKi6NS8Jo"
-
-# Telegram Chat ID (group or personal chat ID)
-CHAT_ID = "1003527140318"
